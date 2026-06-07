89b06823d6422f98563e487a557c3606c18d5c6a
indx0 <111313725+indxx0@users.noreply.github.com>
2026-06-07 18:22:03 +0300

    Clean up formatting in main.cpp
    Removed unnecessary blank lines and fixed formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,8 @@ int main() {
     train.addCar(false);
     train.addCar(true);
     train.addCar(false);
-  
     int length = train.getLength();
-    
     int ops = train.getOpCount();
-
     std::cout << "Train lenght: " << length << std::endl;
     std::cout << "Num of steps" << ops << std::endl;
   
