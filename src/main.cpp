e06765843e09be82584d94e59f230e1b1a8bcc47
indx0 <111313725+indxx0@users.noreply.github.com>
2026-06-07 19:04:04 +0300

    Log train operations to results.csv
    Added functionality to log train operations to a CSV file.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,32 @@
 #include "train.h"
 
 int main() {
-    Train train;
-    train.addCar(false);
-    train.addCar(true);
-    train.addCar(false);
-    train.addCar(false);
-    train.addCar(true);
-    train.addCar(false);
-    int length = train.getLength();
-    int ops = train.getOpCount();
-    std::cout << "Train lenght: " << length << std::endl;
-    std::cout << "Num of steps" << ops << std::endl;
+    std::ofstream outFile("result/results.csv");
+    outFile << "Length,AllOff,AllOn,Random\n";
+
+    const int MAX_LENGTH = 100;
+
+    for (int n = 2; n <= MAX_LENGTH; ++n) {
+        Train train;
+        for (int i = 0; i < n; ++i) {
+            train.addCar(false);
+        }
+        train.resetState(false, false);
+        train.getLength();
+        int opsAllOff = train.getOpCount();
+        train.resetState(true, false);
+        train.getLength();
+        int opsAllOn = train.getOpCount();
+        int opsRandomTotal = 0;
+        int randomRuns = 10;
+        for(int i = 0; i < randomRuns; ++i) {
+            train.resetState(false, true);
+            train.getLength();
+            opsRandomTotal += train.getOpCount();
+        }
+        int opsRandom = opsRandomTotal / randomRuns;
+        outFile << n << "," << opsAllOff << "," << opsAllOn << "," << opsRandom << "\n";
+    }
+    outFile.close();
     return 0;
 }
